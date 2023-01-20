7d65e1b0124e5930d03c1822e6a0f477d2d3872d
Angkoon Junior <angkoon841@gmail.com>
2023-01-20 23:43:09 +0700

    More Progress on Web Port

---
+++ Portfolio-Website-2/src/components/IntroSection.vue
@@ -0,0 +1,247 @@
+<script setup>
+import "bootstrap/js/dist/modal";
+</script>
+
+<template>
+  <div class="introHeader">
+    <!-- <video autoplay loop muted plays-inline></video> -->
+    <div
+      class="back-video"
+      style="
+        width: 100%;
+        padding-bottom: 56.25%;
+        position: absolute;
+        z-index: -1;
+      "
+    >
+      <iframe
+        src="https://streamable.com/e/r0hxra?autoplay=1&nocontrols=1"
+        frameborder="0"
+        width="100%"
+        height="100%"
+        allowfullscreen
+        allow="autoplay"
+        style="
+          width: 100%;
+          height: 100%;
+          position: absolute;
+          left: 0px;
+          top: 0px;
+          overflow: hidden;
+        "
+      >
+      </iframe>
+    </div>
+
+    <nav>
+      <!-- <img src="" alt="Logo" /> -->
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Skills</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+
+    <div class="content">
+      <h1 style="font-style: italic">Angkoon</h1>
+      <br />
+      <h1>Angkoonsawaengsuk</h1>
+      <div id="container">
+        <button class="learn-more">
+          <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+          </span>
+          <span class="button-text" style="color: white">Learn More</span>
+        </button>
+      </div>
+    </div>
+
+    <div class="hangingContent">
+      <p
+        style="
+          color: white;
+          font-size: 15px;
+          font-weight: 500;
+          font-style: italic;
+        "
+      >
+        TM
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Mukta:700')
+
+$bg: #f3f8fa
+$white: #fff
+$black: #282936
+
+@mixin transition($property: all, $duration: 0.45s, $ease: cubic-bezier(0.65,0,.076,1))
+  transition: $property $duration $ease
+
+
+*
+  font-family: 'Mukta', sans-serif
+
+.introHeader
+  width: 100%
+  height: 100vh
+  background-image: linear-gradient(rgba(12,3,51,0.3), rgba(12,3,51,0.3))
+  position: relative
+  padding: 0 5%
+  display: flex
+  align-items: center
+  justify-content: center
+
+.back-video
+  position: absolute
+  right: 0
+  bottom: 0
+  z-index: -1
+
+nav
+  width: 100%
+  position: absolute
+  top: 0
+  left: 0
+  padding: 40px 8%
+  display: flex
+  align-items: center
+  justify-content: space-between
+
+nav
+  ul
+    display: flex
+    align-items: center
+    justify-content: center
+    padding-left: 0px
+    padding-top: auto
+    padding-bottom: auto
+
+nav
+  ul
+    li
+      list-style: none
+      display: inline-block
+      margin-left: 40px
+
+nav
+  ul
+    li
+      a
+        text-decoration: none
+        color: #fff
+        font-size: 25px
+        font-weight: 500
+        transition: all 0.3s ease-in-out
+
+      a:hover
+        text-decoration: none
+        color: red
+
+.content
+  text-align: center
+  animation: slide_Animation 0.5s ease-in-out
+
+.content h1
+  font-size: 80px
+  color: #fff
+  font-weight: 700
+
+#container
+  margin-top: 50px
+
+button
+  position: relative
+  display: inline-block
+  cursor: pointer
+  outline: none
+  border: 0
+  vertical-align: middle
+  text-decoration: none
+  background: transparent
+  padding: 0
+  font-size: inherit
+  font-family: inherit
+
+  &.learn-more
+    width: 12rem
+    height: auto
+    .circle
+      @include transition(all, 0.45s, cubic-bezier(0.65,0,.076,1))
+      position: relative
+      display: block
+      margin: 0
+      width: 3rem
+      height: 3rem
+      background: rgba(234,45,55, 0.8)
+      border-radius: 1.625rem
+      .icon
+        @include transition(all, 0.45s, cubic-bezier(0.65,0,.076,1))
+        position: absolute
+        top: 0
+        bottom: 0
+        margin: auto
+        background: $white
+        &.arrow
+          @include transition(all, 0.45s, cubic-bezier(0.65,0,.076,1))
+          left: 0.625rem
+          width: 1.125rem
+          height: 0.125rem
+          background: none
+          &::before
+            position: absolute
+            content: ''
+            top: -0.25rem
+            right: 0.0625rem
+            width: 0.625rem
+            height: 0.625rem
+            border-top: 0.125rem solid #fff
+            border-right: 0.125rem solid #fff
+            transform: rotate(45deg)
+
+    .button-text
+      @include transition(all, 0.45s, cubic-bezier(0.65,0,.076,1))
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      padding: 0.75rem 0
+      margin: 0 0 0 1.85rem
+      color: $black
+      font-weight: 700
+      line-height: 1.6
+      text-align: center
+      text-transform: uppercase
+
+  &:hover
+    .circle
+      width: 100%
+      .icon
+        &.arrow
+        background: $white
+        transform: translate(1rem, 0)
+
+    .button-text
+      color: $white
+
+// ------------------------------------
+
+@keyframes slide_Animation
+  0%
+    transform: translateY(700px)
+  100%
+    transform: translateY(0)
+</style>
--- Portfolio-Website/src/App.vue
+++ Portfolio-Website/src/App.vue
@@ -3,25 +3,51 @@ import "bootstrap/js/dist/modal";
 </script>
 
 <template>
-  <div class="row">
-    <div class="bg">
-      <img
-        class="background"
-        src="https://i.ibb.co/QHfMKYf/shutterstock-2104429805.jpg"
-        alt="bg-image"
-      />
+  <div class="mainDiv">
+    <section class="headerSection">
       <div class="row">
-        <div class="col-6 leftCard">
-          <div class="mainText">
-            <span style="font-style: italic">ANGKOON</span>
-            <br />
-            <span>ANGKOONSAWAENGSUK</span>
+        <div class="bg">
+          <img
+            class="background"
+            src="https://i.ibb.co/QHfMKYf/shutterstock-2104429805.jpg"
+            alt="bg-image"
+          />
+        </div>
+        <div class="row container" style="padding: 0px">
+          <div id="marketing" class="section" style="padding: 0px">
+            <div class="content">
+              <h1>Main</h1>
+            </div>
+            <div class="overlay"></div>
+          </div>
+
+          <div id="marketing" class="section" style="padding: 0px">
+            <div class="content">
+              <h1>Skills</h1>
+            </div>
+            <div class="overlay"></div>
+          </div>
+
+          <div id="marketing" class="section" style="padding: 0px">
+            <div class="content">
+              <h1>Work</h1>
+            </div>
+            <div class="overlay"></div>
+          </div>
+
+          <div id="technology" class="section" style="padding: 0px">
+            <div class="content">
+              <h1>Contact</h1>
+            </div>
+            <div class="overlay"></div>
           </div>
         </div>
-
-        <div class="col-6 rightCard"></div>
       </div>
-    </div>
+    </section>
+
+    <section class="skillSection">
+
+    </section>
   </div>
 </template>
 
@@ -36,7 +62,81 @@ export default {
 </script>
 
 <style lang="sass">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap')
+@import url("https://fonts.googleapis.com/css?family=Oswald|Noto+Sans")
+
+*
+  box-sizing: border-box
+
+.container
+  display: flex
+  height: 100vh
+  margin-right: 0px !important
+  margin-left: 0px !important
+  max-width: 100% !important
+
+.section
+  flex: 1
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100%
+  overflow: hidden
+  background-size: cover
+  background-position: center
+  color: black
+  transition: flex 0.4s ease
+  position: relative
+  border: 1px solid rgba(255, 255, 255, 0.85)
+  padding: 0px
+
+.section .overlay
+  background: rgba(255, 255, 255, 0.5)
+  width: 100%
+  height: 100%
+  position: absolute
+  transition: background-color 0.8s ease
+
+.section .content
+  z-index: 2
+  writing-mode: vertical-rl
+  text-orientation: upright
+
+.section:hover:first-child
+  flex: 20
+  justify-content: left
+
+  .content h1
+    font-size: 100px
+
+.section:hover:nth-child(2)
+  flex: 12
+  justify-content: left
+
+  .content h1
+    font-size: 100px
+
+.section:hover:nth-child(3), .section:hover:last-child
+  flex: 12
+  justify-content: right
+
+  .content h1
+    font-size: 100px
+
+.section:hover .overlay
+  background-color: rgba(255, 255, 255, 0.85)
+  // background-image: url('https://i.ibb.co/DrTVctz/css-particles-background.gif')
+  background-repeat: repeat-y
+  background-attachment: fixed
+
+#marketing
+  // background-image: url(https://images.unsplash.com/photo-1522205987242-8e22924ab42a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c0f679eb8f15705d46ea90008f39642b&auto=format&fit=crop&w=500&q=60)
+  background-image: url('http://127.0.0.1:8887/css-particles-background.gif')
+
+// #technology
+//   background-image: url(https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fe6b92f88f55824e64a1bae15f5bf52a&auto=format&fit=crop&w=500&q=60)
+
+// #advertising
+//   background-image: url(https://images.unsplash.com/photo-1517799094725-e3453440724e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=353f3663a9fae75773d2942aeb37c2c8&auto=format&fit=crop&w=500&q=60)
 
 .bg
   img
@@ -53,22 +153,22 @@ export default {
     z-index: -1
     position: absolute
 
-.leftCard
-  background-color: white
-  background-color: rgba(255, 255, 255, 0.2)
-  box-shadow: 0 15px 25px rgba(129, 124, 124, 0.2)
-  backdrop-filter: blur(14px)
-  color: black
-  position: absolute
-  z-index: 2
-  width: 100vh
-  height: 100vh
-  
-  .mainText
-    font-family: 'Montserrat', sans-serif
-    font-size: 6vh
-    font-weight: 1000 
-    text-align: left
-    margin-left: 5.5vh
-    padding: 70% 0px 
+// .leftCard
+//   background-color: white
+//   background-color: rgba(255, 255, 255, 0.2)
+//   box-shadow: 0 15px 25px rgba(129, 124, 124, 0.2)
+//   backdrop-filter: blur(14px)
+//   color: black
+//   position: absolute
+//   z-index: 2
+//   width: 100vh
+//   height: 100vh
+
+//   .mainText
+//     font-family: "Noto Sans", sans-serif
+//     font-size: 6.5vh
+//     font-weight: 700
+//     text-align: left
+//     margin-left: 5.5vh
+//     padding: 70% 0px
 </style>
